71cefec87a8badfa1c7dec03ece8cd41567296f9
Sebastian Streng <51957686+SebastianStreng@users.noreply.github.com>
2022-10-03 21:12:57 +0200

    Update Program.cs

--- src/StartProject/Program.cs
+++ src/StartProject/Program.cs
@@ -3,8 +3,24 @@ public class Program
 {
     static void Main(string[] args)
     {
-        SingleResponsibilityPrinciple.goodCode.SRPGoodCode.GetGoodCode(); 
+        SingleResponsibilityPrinciple.goodCode.SRPGoodCode.GetGoodCode();
+
+        string path = $"{Directory.GetCurrentDirectory()}/test.txt";
+        Console.Write(DoSomethingWithThisContent(GetConentFromTxtFile(path)));
 
     }
+
+    public static string GetConentFromTxtFile(string path)
+    {
+        return System.IO.File.ReadAllText(path);
+    }
+
+    public static string DoSomethingWithThisContent(string content)
+    {
+        return content; 
+    }
+
+
+
 }
 
